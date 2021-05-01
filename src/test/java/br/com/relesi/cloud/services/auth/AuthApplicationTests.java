5ab74d1b32e7308949aebb48e6f168240737af62
relesi <renatolessa.2011@hotmail.com>
2021-05-01 07:55:16 -0300

    This provider implements the Authorization Code

---
+++ src/test/java/br/com/relesi/cloud/services/auth/AuthApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.relesi.cloud.services.auth;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
